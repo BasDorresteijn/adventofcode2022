a787e6207e7b9123a69c9865b8ba7a823277af57
Bas Dorresteijn <basd1998@gmail.com>
2022-12-04 17:36:28 +0100

    Add day 4 (javascript)

---
+++ day4/program.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('day4/file.txt', { encoding: 'utf8', flag: 'r' });
+const result1 = data.split('\n').filter(x => {
+    const sections = x.split(',');
+    const sectionA = sections[0].split('-');
+    const sectionB = sections[1].split('-');
+    const minContainRangeA = parseInt(sectionA[0], 10);
+    const maxContainRangeA = parseInt(sectionA[1], 10);
+    const minContainRangeB = parseInt(sectionB[0], 10);
+    const maxContainRangeB = parseInt(sectionB[1], 10);
+    return (minContainRangeA <= minContainRangeB && maxContainRangeA >= maxContainRangeB) || (minContainRangeB <= minContainRangeA && maxContainRangeB >= maxContainRangeA)
+})
+
+console.log('Answer 1: ', result1.length);
+
+const result2 = data.split('\n').filter(x => {
+    const sections = x.split(',');
+    const sectionA = sections[0].split('-');
+    const sectionB = sections[1].split('-');
+    const minContainRangeA = parseInt(sectionA[0], 10);
+    const maxContainRangeA = parseInt(sectionA[1], 10);
+    const minContainRangeB = parseInt(sectionB[0], 10);
+    const maxContainRangeB = parseInt(sectionB[1], 10);
+    return (minContainRangeA >= minContainRangeB && minContainRangeA <= maxContainRangeB) || (minContainRangeB >= minContainRangeA && minContainRangeB <= maxContainRangeA) ||
+        (minContainRangeB >= minContainRangeA && minContainRangeB <= maxContainRangeA) || (minContainRangeA >= minContainRangeB && minContainRangeA <= maxContainRangeB)
+})
+
+console.log('Answer 2: ', result2.length);
