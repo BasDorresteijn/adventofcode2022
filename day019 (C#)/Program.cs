7520fba8e4c2f0aacab71599d1d121b72c5ae650
Bas Dorresteijn <basd1998@gmail.com>
2023-01-03 21:15:16 +0100

    Add day 19 part 2 (slow!)

--- day019 (C#)/Program.cs
+++ day019 (C#)/Program.cs
@@ -3,53 +3,121 @@ List<MineralMiningExpedition> Expeditions = new List<MineralMiningExpedition>();
 
 var input = File.ReadAllText(@"file.txt");
 var splitInput = input.Split("\r\n");
-var total = 0;
 var readRowRegex = new Regex("Blueprint \\d*: Each ore robot costs (\\d*) ore. Each clay robot costs (\\d*) ore. Each obsidian robot costs (\\d*) ore and (\\d*) clay. Each geode robot costs (\\d*) ore and (\\d*) obsidian\\.");
 
-for (int i = 0; i < splitInput.Length; i++)
+Program1();
+Program2();
+
+void Program1()
 {
-    var row = splitInput[i];
-    var regexResult = readRowRegex.Match(row);
+    var total = 0;
+    for (int i = 0; i < splitInput!.Length; i++)
+    {
+        var row = splitInput[i];
+        var regexResult = readRowRegex!.Match(row);
 
-    var startItem1 = new MineralMiningExpedition()
-    {
-        NextMiner = MinerType.Ore,
-        Expeditions = Expeditions,
-        OreMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(1).Value),
-        ClayMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(2).Value),
-        ObsidianMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(3).Value),
-        ObsidianMinerClayCost = int.Parse(regexResult.Groups.Values.ElementAt(4).Value),
-        GeodeMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(5).Value),
-        GeodeMinerObsidianCost = int.Parse(regexResult.Groups.Values.ElementAt(6).Value)
-    };
-    var startItem2 = new MineralMiningExpedition()
-    {
-        NextMiner = MinerType.Clay,
-        Expeditions = Expeditions,
-        OreMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(1).Value),
-        ClayMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(2).Value),
-        ObsidianMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(3).Value),
-        ObsidianMinerClayCost = int.Parse(regexResult.Groups.Values.ElementAt(4).Value),
-        GeodeMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(5).Value),
-        GeodeMinerObsidianCost = int.Parse(regexResult.Groups.Values.ElementAt(6).Value)
-    };
-    Expeditions.Add(startItem1);
-    Expeditions.Add(startItem2);
+        var costs = new List<int?> { int.Parse(regexResult.Groups.Values.ElementAt(1).Value), int.Parse(regexResult.Groups.Values.ElementAt(2).Value), int.Parse(regexResult.Groups.Values.ElementAt(3).Value), int.Parse(regexResult.Groups.Values.ElementAt(5).Value) };
+        int? maxOreCost = costs.Max();
 
-    for (int j = 0; j < 24; j++)
-    {
-        Expeditions.ToList().ForEach(x => x.DoCycle());
+        var startItem1 = new MineralMiningExpedition()
+        {
+            NextMiner = MinerType.Ore,
+            MaxOreCost = maxOreCost!.Value,
+            Expeditions = Expeditions,
+            OreMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(1).Value),
+            ClayMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(2).Value),
+            ObsidianMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(3).Value),
+            ObsidianMinerClayCost = int.Parse(regexResult.Groups.Values.ElementAt(4).Value),
+            GeodeMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(5).Value),
+            GeodeMinerObsidianCost = int.Parse(regexResult.Groups.Values.ElementAt(6).Value)
+        };
+        var startItem2 = new MineralMiningExpedition()
+        {
+            NextMiner = MinerType.Clay,
+            MaxOreCost = maxOreCost!.Value,
+            Expeditions = Expeditions,
+            OreMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(1).Value),
+            ClayMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(2).Value),
+            ObsidianMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(3).Value),
+            ObsidianMinerClayCost = int.Parse(regexResult.Groups.Values.ElementAt(4).Value),
+            GeodeMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(5).Value),
+            GeodeMinerObsidianCost = int.Parse(regexResult.Groups.Values.ElementAt(6).Value)
+        };
+
+        Expeditions.Add(startItem1);
+        Expeditions.Add(startItem2);
+
+        for (int j = 0; j < 24; j++)
+        {
+            Console.WriteLine("Progress: " + j + "/24");
+            Expeditions.ToList().ForEach(x => x.DoCycle());
+        }
+
+        var highestCount = Expeditions.Max(x => x.Geode);
+        var subResult = (i + 1) * highestCount;
+        Console.WriteLine($"Id {i + 1}, Geodes {highestCount}, SubResult {subResult}");
+        total += subResult;
+
+        Expeditions.Clear();
     }
 
-    var highestCount = Expeditions.Max(x => x.Geode);
-    var subResult = (i + 1) * highestCount;
-    Console.WriteLine($"Id {i + 1}, Geodes {highestCount}, SubResult {subResult}");
-    total += subResult;
-
-    Expeditions.Clear();
+    Console.WriteLine($"Answer 1: {total}");
 }
 
-Console.WriteLine($"Answer 1: {total}");
+void Program2()
+{
+    var total = 1;
+    for (int i = 0; i < splitInput!.Length; i++)
+    {
+        var row = splitInput[i];
+        var regexResult = readRowRegex!.Match(row);
+
+        var costs = new List<int?> { int.Parse(regexResult.Groups.Values.ElementAt(1).Value), int.Parse(regexResult.Groups.Values.ElementAt(2).Value), int.Parse(regexResult.Groups.Values.ElementAt(3).Value), int.Parse(regexResult.Groups.Values.ElementAt(5).Value) };
+        int? maxOreCost = costs.Max();
+
+        var startItem1 = new MineralMiningExpedition()
+        {
+            NextMiner = MinerType.Ore,
+            MaxOreCost = maxOreCost!.Value,
+            Expeditions = Expeditions,
+            OreMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(1).Value),
+            ClayMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(2).Value),
+            ObsidianMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(3).Value),
+            ObsidianMinerClayCost = int.Parse(regexResult.Groups.Values.ElementAt(4).Value),
+            GeodeMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(5).Value),
+            GeodeMinerObsidianCost = int.Parse(regexResult.Groups.Values.ElementAt(6).Value)
+        };
+        var startItem2 = new MineralMiningExpedition()
+        {
+            NextMiner = MinerType.Clay,
+            MaxOreCost = maxOreCost!.Value,
+            Expeditions = Expeditions,
+            OreMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(1).Value),
+            ClayMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(2).Value),
+            ObsidianMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(3).Value),
+            ObsidianMinerClayCost = int.Parse(regexResult.Groups.Values.ElementAt(4).Value),
+            GeodeMinerOreCost = int.Parse(regexResult.Groups.Values.ElementAt(5).Value),
+            GeodeMinerObsidianCost = int.Parse(regexResult.Groups.Values.ElementAt(6).Value)
+        };
+
+        Expeditions.Add(startItem1);
+        Expeditions.Add(startItem2);
+
+        for (int j = 0; j < 32; j++)
+        {
+            Console.WriteLine("Progress: " + j + "/32");
+            Expeditions.ToList().ForEach(x => x.DoCycle());
+        }
+
+        var highestCount = Expeditions.Max(x => x.Geode);
+        Console.WriteLine($"Id {i + 1}, Geodes {highestCount}");
+        total *= highestCount;
+
+        Expeditions.Clear();
+    }
+
+    Console.WriteLine($"Answer 2: {total}");
+}
 
 enum MinerType
 {
@@ -62,6 +130,7 @@ enum MinerType
 class MineralMiningExpedition
 {
     public List<MineralMiningExpedition> Expeditions { init; private get; } = new List<MineralMiningExpedition>();
+    public int MaxOreCost { init; private get; }
     public MinerType NextMiner { init; private get; }
     public Action BuildMiner = () => { };
     private int CycleCount;
@@ -84,21 +153,14 @@ class MineralMiningExpedition
     public int GeodeMinerOreCost { init; private get; }
     public int GeodeMinerObsidianCost { init; private get; }
 
-    public override string ToString()
-    {
-        return @$"
-            Ores: {Ore} | {OreMinerCount}
-            Clay: {Clay} | {ClayMinerCount}
-            Obsidian: {Obsidian} | {ObsidianMinerCount}
-            Geode: {Geode} | {GeodeMinerCount}
-            CycleCount: {CycleCount}
-        ";
-    }
-
-    public string Log = "";
-
     public void DoCycle()
     {
+        if (OreMinerCount > MaxOreCost || ClayMinerCount > ObsidianMinerClayCost || ObsidianMinerCount > GeodeMinerObsidianCost)
+        {
+            Expeditions.Remove(this);
+            return;
+        }
+
         CycleCount++;
         TryBuyMiner();
         UpdateResources();
@@ -115,10 +177,7 @@ class MineralMiningExpedition
 
     private void TryBuyMiner()
     {
-        BuildMiner = () =>
-        {
-            Log += ToString();
-        };
+        BuildMiner = () => { };
         switch (NextMiner)
         {
             case MinerType.Ore:
@@ -128,7 +187,6 @@ class MineralMiningExpedition
                     {
                         Ore -= OreMinerOreCost;
                         OreMinerCount++;
-                        Log += ToString();
                         AddScenarios();
                     };
                 }
@@ -140,7 +198,6 @@ class MineralMiningExpedition
                     {
                         Ore -= ClayMinerOreCost;
                         ClayMinerCount++;
-                        Log += ToString();
                         AddScenarios();
                     };
                 }
@@ -153,7 +210,6 @@ class MineralMiningExpedition
                         Ore -= ObsidianMinerOreCost;
                         Clay -= ObsidianMinerClayCost;
                         ObsidianMinerCount++;
-                        Log += ToString();
                         AddScenarios();
                     };
                 }
@@ -166,7 +222,6 @@ class MineralMiningExpedition
                         Ore -= GeodeMinerOreCost;
                         Obsidian -= GeodeMinerObsidianCost;
                         GeodeMinerCount++;
-                        Log += ToString();
                         AddScenarios();
                     };
                 }
@@ -176,32 +231,35 @@ class MineralMiningExpedition
 
     private void AddScenarios()
     {
-        if (NextMiner != MinerType.Ore)
+        if (CycleCount > 20 && (ObsidianMinerCount < 0 || GeodeMinerCount < 0))
         {
-            var costs = new List<int?> { OreMinerOreCost, ClayMinerOreCost, ObsidianMinerOreCost, GeodeMinerOreCost };
-            int? maxOreCost = costs.Max();
-            if (OreMinerCount < maxOreCost)
-            {
-                AddScenario(MinerType.Ore);
-            }
+            Expeditions.Remove(this);
+            return;
         }
 
+        if (NextMiner != MinerType.Ore && OreMinerCount < MaxOreCost)
+            AddScenario(MinerType.Ore);
         if (NextMiner != MinerType.Clay && ClayMinerCount < ObsidianMinerClayCost)
             AddScenario(MinerType.Clay);
         if (ClayMinerCount > 0 && NextMiner != MinerType.Obsidian && ObsidianMinerCount < GeodeMinerObsidianCost)
             AddScenario(MinerType.Obsidian);
-
-
         if (ObsidianMinerCount > 0 && NextMiner != MinerType.Geode)
             AddScenario(MinerType.Geode);
     }
 
     private void AddScenario(MinerType minerType)
     {
+        if (Expeditions.Any(x => x.Geode >= Geode && x.GeodeMinerCount >= GeodeMinerCount && x.Obsidian >= Obsidian && x.ObsidianMinerCount >= ObsidianMinerCount &&
+            x.Ore >= Ore && x.OreMinerCount >= OreMinerCount && x.Clay >= Clay && x.ClayMinerCount >= ClayMinerCount && x.CycleCount <= CycleCount && x.NextMiner == minerType
+        ))
+        {
+            return;
+        }
+
         Expeditions.Add(new MineralMiningExpedition()
         {
-            Log = Log,
             NextMiner = minerType,
+            MaxOreCost = MaxOreCost,
             Expeditions = Expeditions,
             CycleCount = CycleCount,
             Ore = Ore,
